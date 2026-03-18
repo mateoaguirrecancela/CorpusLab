332458ddc8679f8dce849d5069f03d16edae8b3b
Mateo Aguirre Cancela <mateo.aguirre@udc.es>
2026-03-18 21:09:22 +0100

    first commit

---
+++ backend/src/main/java/es/udc/fic/corpuslab/CorpusLabApplication.java
@@ -0,0 +1,13 @@
+package es.udc.fic.corpuslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpusLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpusLabApplication.class, args);
+	}
+
+}
